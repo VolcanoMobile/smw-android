0d64f5754bfa9cc1c1aaadc2208a0b3c5310d5c8
Philippe Simons <philippe.simons@volcanomobile.net>
2020-03-04 16:34:56 +0100

    Fix maps in worlds folder loading

--- app/src/main/jni/src/common/MapList.h
+++ app/src/main/jni/src/common/MapList.h
@@ -1,63 +1,61 @@
 #ifndef MAPLIST_H
 #define MAPLIST_H
 
-#include "path.h"
-
 #include <map>
 #include <string>
 
 class MapListNode
 {
-	public:
-		MapListNode(std::string fullName);
-		~MapListNode();
+public:
+    MapListNode(std::string fullName);
+    ~MapListNode();
 
-		bool * pfFilters;
-		bool fInCurrentFilterSet;
-		std::string filename;
-		short iIndex;
-		short iFilteredIndex;
+    bool * pfFilters;
+    bool fInCurrentFilterSet;
+    std::string filename;
+    short iIndex;
+    short iFilteredIndex;
 
-		int iShortNameLength;
+    int iShortNameLength;
 
-		bool fReadFromCache;
+    bool fReadFromCache;
 
-		bool fValid;
+    bool fValid;
 };
 
 //announcerlist and musiclist are still a screwed up (a vector accessed like a vector and a list), but way better than before
 class MapList
 {
-    public:
-        MapList(bool fWorldEditor);
-        ~MapList();
+public:
+    MapList(bool fWorldEditor);
+    ~MapList();
 
-		//Adds maps in all world map directories to the map list so they can be edited in the map editor
-		void addWorldMaps();
+    //Adds maps in all world map directories to the map list so they can be edited in the map editor
+    void addWorldMaps();
 
-        void add(const char * name);
-        bool find(const char * name);
-		bool findexact(const char * name, bool fWorld);
-        bool startswith(char letter);
-		bool startswith(std::string match);
+    void add(const char * name);
+    bool find(const char * name);
+    bool findexact(const char * name, bool fWorld);
+    bool startswith(char letter);
+    bool startswith(std::string match);
 
     const char* currentFilename() {
         return (*outercurrent).second->filename.c_str();
     }
     const char* currentShortmapname() {
-        return stripCreatorAndDotMap(outercurrent->first).c_str();
+        return (*outercurrent).first.c_str();
     }
     int currentShortMapNameLen() {
         return (*outercurrent).second->iShortNameLength;
     }
 
-		void prev(bool fUseFilters);
-        void next(bool fUseFilters);
-		void random(bool fUseFilters);
+    void prev(bool fUseFilters);
+    void next(bool fUseFilters);
+    void random(bool fUseFilters);
 
-		const char* randomFilename();
+    const char* randomFilename();
 
-		//Sets/Gets if a map at the current map node is valid and can be loaded
+    //Sets/Gets if a map at the current map node is valid and can be loaded
     void SetValid(bool fValid) {
         (*current).second->fValid = fValid;
     }
@@ -65,7 +63,7 @@ class MapList
         return (*current).second->fValid;
     }
 
-		//TODO: use size?
+    //TODO: use size?
     bool IsEmpty() {
         return maps.empty();
     }
@@ -77,15 +75,15 @@ class MapList
         return maps.size();
     }
 
-    std::map<std::string, MapListNode*>::iterator GetCurrent() {
+    std::multimap<std::string, MapListNode*>::iterator GetCurrent() {
         return current;
     }
-    void SetCurrent(std::map<std::string, MapListNode*>::iterator itr) {
+    void SetCurrent(std::multimap<std::string, MapListNode*>::iterator itr) {
         outercurrent = current = itr;
     }
 
-		void WriteFilters();
-		void ReadFilters();
+    void WriteFilters();
+    void ReadFilters();
 
     bool GetFilter(short iFilter) {
         return (*current).second->pfFilters[iFilter];
@@ -97,11 +95,11 @@ class MapList
         (*current).second->pfFilters[iFilter] = !(*current).second->pfFilters[iFilter];
     }
 
-		bool FindFilteredMap();
-		void ApplyFilters(bool * pfFilters);
-		bool MapInFilteredSet();
+    bool FindFilteredMap();
+    void ApplyFilters(bool * pfFilters);
+    bool MapInFilteredSet();
 
-		std::map<std::string, MapListNode*>::iterator GetIteratorAt(unsigned short iIndex, bool fUseFilters);
+    std::multimap<std::string, MapListNode*>::iterator GetIteratorAt(unsigned short iIndex, bool fUseFilters);
 
     void SaveCurrent() {
         savedcurrent = current;
@@ -110,38 +108,27 @@ class MapList
         current = savedcurrent;
     }
 
-		void ReloadMapAutoFilters();
-		void WriteMapSummaryCache();
+    void ReloadMapAutoFilters();
+    void WriteMapSummaryCache();
 
-		const char * GetUnknownMapName();
+    const char * GetUnknownMapName();
 
-    private:
-        struct CompareShortName {
-            bool operator()(const std::string& a, const std::string& b) const {
-                const std::string a_short = stripCreatorAndDotMap(a);
-                const std::string b_short = stripCreatorAndDotMap(b);
-                if (a_short != b_short)
-                    return a_short < b_short;
+private:
 
-                return a < b;
-            }
-        };
+    std::multimap<std::string, MapListNode*> maps;
+    std::multimap<std::string, MapListNode*> worldmaps;
 
+    std::multimap<std::string, MapListNode*>::iterator current;
+    std::multimap<std::string, MapListNode*>::iterator savedcurrent;
 
-        std::map<std::string, MapListNode*, CompareShortName> maps;
-		std::map<std::string, MapListNode*, CompareShortName> worldmaps;
+    std::multimap<std::string, MapListNode*>::iterator outercurrent;
 
-        std::map<std::string, MapListNode*, CompareShortName>::iterator current;
-		std::map<std::string, MapListNode*, CompareShortName>::iterator savedcurrent;
+    short iFilteredMapCount;
 
-		std::map<std::string, MapListNode*, CompareShortName>::iterator outercurrent;
+    std::multimap<std::string, MapListNode*>::iterator * mlnFilteredMaps;
+    std::multimap<std::string, MapListNode*>::iterator * mlnMaps;
 
-		short iFilteredMapCount;
-
-		std::map<std::string, MapListNode*, CompareShortName>::iterator * mlnFilteredMaps;
-		std::map<std::string, MapListNode*, CompareShortName>::iterator * mlnMaps;
-
-		char szUnknownMapString[2];
+    char szUnknownMapString[2];
 };
 
 #endif // MAPLIST_H
